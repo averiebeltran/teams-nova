0e391a4c5745fafd4710e7f0130f540cd0769a0f
AverieBeltran <averiebeltran@github.com>
2021-02-14 12:32:36 -0600

    refactoring and bootstrap

--- src/badge.js
+++ src/badge.js
@@ -1,33 +1,21 @@
 class Badge {
     constructor(teamMember) {
-        this.template = `
-            <div class="badge">
-                <div class="name">
-                    ` + teamMember.name + `
-                </div>
-                <ul class="info">
-                    <li>ID: ` + teamMember.id + `</li>
-                    <li>Email: ` + teamMember.email + `</li>
-                    <li>` + this.getFinalListItem(teamMember) + `<li>
-                </ul>
-            </div>
-        `;
+        console.log(teamMember);
 
-        // console.log(this.template);
-    }
-
-    getFinalListItem(teamMember) {
         switch(teamMember.constructor.name){
             case "Manager":
-                `Office number: ` + teamMember.officeNumber;
+                this.finalListItem = "Office number: " + teamMember.officeNumber;
                 break;
             case "Intern":
-                `School: ` + teamMember.school;
+                this.finalListItem = "School: " + teamMember.school;
                 break;
             case "Engineer":
-                `Github: ` + teamMember.github;
+                this.finalListItem = "Github: " + teamMember.github;
                 break;
         }
+
+        this.template = "<div class='badge card col-3'><div class='name card-header'>" + teamMember.name + "<br>" + teamMember.constructor.name + "</div><ul class='info list-group list-group-flush'><li class='list-group-item'>ID: " + teamMember.id + "</li><li class='list-group-item'>Email: " + teamMember.email + "</li><li class='list-group-item'>" + this.finalListItem + "</li></ul></div>";
+        console.log(this.template);
     }
 
 }
